5be289e56c5bf884633c77aa44326e503f075370
Meliton Tienda Avila <mtienda@geoit.com.mx>
2024-06-19 12:14:49 -0600

    Se corrigen días de pronostico del tiempo

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -8,7 +8,7 @@ const Weather = ((element) => {
     dayContainer.click(onDayContainerClick);
     weatherCharts.click(onWeatherChartsClick);
 
-    onDayContainerClick();
+    // onDayContainerClick();
   }
 
   function onWeatherChartsClick() {
